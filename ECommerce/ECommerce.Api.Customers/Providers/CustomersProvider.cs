0859cefa488a8ef5d020034bdf29b9da422df541
abhay11242004 <abhaypatel274@gmail.com>
2024-11-30 21:20:51 -0500

    Implemented customer and order

---
+++ ECommerce/ECommerce.Api.Customers/Providers/CustomersProvider.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Providers
+{
+    public class CustomersProvider : ICustomersProvider
+    {
+        private readonly CustomersDbContext _dbContext;
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+        public CustomersProvider(CustomersDbContext dbContext, ILogger<CustomersProvider> logger, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+            _mapper = mapper;
+            SeedData();
+        }
+        private void SeedData()
+        {
+            if (!_dbContext.Customers.Any())
+            {
+                _dbContext.Add(new Db.Customer() { Id = 1, Name = "Abhay", Address = "123 street" });
+                _dbContext.Add(new Db.Customer() { Id = 2, Name = "Rambert", Address = "456 street" });
+                _dbContext.Add(new Db.Customer() { Id = 3, Name = "Julien", Address = "789 street" });
+                _dbContext.SaveChanges();
+            }
+        }
+        public async Task<(bool IsSuccess, IEnumerable<Models.Customer> Customers, string ErrorMesssage)> GetCustomersAsync()
+        {
+            try
+            {
+                _logger?.LogInformation("Query customers");
+                var customers = await _dbContext.Customers.ToListAsync();
+                if(customers != null && customers.Any())
+                {
+                    _logger?.LogInformation($"{customers.Count} customer(s)");
+                    var result = _mapper.Map<IEnumerable<Db.Customer>, IEnumerable<Models.Customer>>(customers);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch(Exception ex)
+            {
+                _logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool IsSuccess, Models.Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                _logger?.LogInformation("Query customers");
+                var customer = await _dbContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+                if (customer != null)
+                {
+                    _logger?.LogInformation("Customer found");
+                    var result = _mapper.Map<Db.Customer, Models.Customer>(customer);
+                    return (true, result, null);
+                }
+                return (false, null, "Not found");
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
--- ECommerce/ECommerce.Api.Products/obj/Debug/netcoreapp3.1/ECommerce.Api.Products.AssemblyInfo.cs
+++ ECommerce/ECommerce.Api.Products/obj/Debug/netcoreapp3.1/ECommerce.Api.Products.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02698acba31ca9093e422c7028bbf659fd4c9368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813d72e5ad5a636daf98edac3890b8bed16da417")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
