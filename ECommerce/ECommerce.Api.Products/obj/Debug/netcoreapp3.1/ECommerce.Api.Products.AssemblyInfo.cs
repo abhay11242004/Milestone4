c844c5d8c43d07fe296029e1b710e8cc8c836e72
abhay11242004 <abhaypatel274@gmail.com>
2024-11-30 21:52:14 -0500

    implemented search service

--- ECommerce/ECommerce.Api.Products/obj/Debug/netcoreapp3.1/ECommerce.Api.Products.AssemblyInfo.cs
+++ ECommerce/ECommerce.Api.Products/obj/Debug/netcoreapp3.1/ECommerce.Api.Products.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813d72e5ad5a636daf98edac3890b8bed16da417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0859cefa488a8ef5d020034bdf29b9da422df541")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api.Products")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
