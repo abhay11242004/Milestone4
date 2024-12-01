c453f846400e8eb8d03fa569b4be7e1bb58cd0df
abhay11242004 <abhaypatel274@gmail.com>
2024-11-30 22:08:51 -0500

    invoked orders from search

--- ECommerce/ECommerce.Api.Search/Services/SearchService.cs
+++ ECommerce/ECommerce.Api.Search/Services/SearchService.cs
@@ -5,10 +5,23 @@ namespace ECommerce.Api.Search.Services
 {
     public class SearchService : ISearchService
     {
+        private readonly IOrdersService _ordersService;
+        public SearchService(IOrdersService orderService)
+        {
+            _ordersService = orderService;
+        }
         public async Task<(bool IsSuccess, dynamic SearchResults)> SearchAsync(int customerId)
         {
-            await Task.Delay(1);
-            return (true, new { Message = "Hello" });
+            var ordersResult = await _ordersService.GetOrderAsync(customerId);
+            if (ordersResult.IsSuccess)
+            {
+                var result = new
+                {
+                    Orders = ordersResult.Orders
+                };
+                return (true, result);
+            }
+            return (false, null);
         }
     }
 }
--- ECommerce/ECommerce.Api.Search/obj/Debug/netcoreapp3.1/ECommerce.Api.Search.AssemblyInfo.cs
+++ ECommerce/ECommerce.Api.Search/obj/Debug/netcoreapp3.1/ECommerce.Api.Search.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Search")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0859cefa488a8ef5d020034bdf29b9da422df541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c844c5d8c43d07fe296029e1b710e8cc8c836e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api.Search")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api.Search")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
